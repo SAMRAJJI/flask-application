525b9da8e63a7b04a8763fe13c93f2173c61b0c3
samrajji <nsraj1713@gmail.com>
2025-12-24 15:05:36 +0530

    updated

--- myapp/static/index.js
+++
@@ -1,9 +0,0 @@
-function deletenote(noteid){
-    fetch('/delete-note',{
-        method:'POST',
-        body: JSON.stringify({noteid:noteid})
-    }).then((res)=>{
-        window.location.href='/';
-    });
-
-}
--- myapp/views.py
+++ myapp/views.py
@@ -8,16 +8,6 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST':
-        notes = request.form.get('notes')
-
-        if len(notes) < 1:
-            flash('Notes is too short!', category = 'error')
-        else:
-            new_notes = Note(data = notes, user_id = current_user.id)
-            db.session.add(new_notes)
-            db.session.commit()
-            flash('notes is added', category = 'success')
     return render_template("home.html", user = current_user)
 
 @views.route('/dashboard')
@@ -26,16 +16,3 @@ def dashboard():
     return render_template("dashboard.html", user = current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
-@login_required
-def delete_note():
-    data = json.loads(request.data)
-    note_id = data.get('noteId')
-
-    note = Note.query.get(note_id)
-
-    if note and note.user_id == current_user.id:
-        db.session.delete(note)
-        db.session.commit()
-
-    return jsonify({})
