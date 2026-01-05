1f96044444f8ad8a468da1a9a2037b49f5e68af9
samrajji <nsraj1713@gmail.com>
2026-01-05 19:18:14 +0530

    updated

--- myapp/models.py
+++ myapp/models.py
@@ -1,20 +1,21 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from datetime import datetime
 
-class Note(db.Model):
-    id =  db.Column(db.Integer, primary_key =  True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone = True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(156), unique= True)
     password =  db.Column(db.String(120))
     name = db.Column(db.String(120))
-    notes = db.relationship('Note')
 
-
+    blogs = db.relationship('Blog', backref='author', lazy=True)
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,18 +1,66 @@
-from flask import Blueprint, render_template, flash, request, jsonify
+from flask import Blueprint, render_template, flash, request, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Blog
 from . import db
-import json
+
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template("home.html", user = current_user)
+    return render_template("home.html", user=current_user)
 
 @views.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template("dashboard.html", user = current_user)
+    return render_template("dashboard.html", user=current_user)
 
+# ---------------- BLOG ROUTES ---------------- #
 
+@views.route('/blogs')
+@login_required
+def blogs():
+    posts = Blog.query.order_by(Blog.created_at.desc()).all()
+    return render_template("blogs.html", posts=posts)
+
+@views.route('/blog/add', methods=['GET', 'POST'])
+@login_required
+def add_blog():
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        new_blog = Blog(
+            title=title,
+            content=content,
+            user_id=current_user.id
+        )
+
+        db.session.add(new_blog)
+        db.session.commit()
+
+        flash("Blog added successfully!", "success")
+        return redirect(url_for('views.blogs'))
+
+    return render_template("add_blog.html")
+
+@views.route('/blog/<int:id>')
+@login_required
+def view_blog(id):
+    post = Blog.query.get_or_404(id)
+    return render_template("view_blog.html", post=post)
+
+@views.route('/blog/delete/<int:id>')
+@login_required
+def delete_blog(id):
+    post = Blog.query.get_or_404(id)
+
+    if post.user_id != current_user.id:
+        flash("Unauthorized access!", "danger")
+        return redirect(url_for('views.blogs'))
+
+    db.session.delete(post)
+    db.session.commit()
+    flash("Blog deleted!", "success")
+
+    return redirect(url_for('views.blogs'))
